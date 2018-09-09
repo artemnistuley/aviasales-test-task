76695600545f0c8a67f159eb22a575532d29195b
Artem Nistuley <artemnistuley@MacBook-Pro-Artem.local>
2018-09-09 17:53:55 +0300

    Changed filtering (#task2)

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,43 +4,56 @@ class Filter extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      all: false,
+      0: false,
+      1: false,
+      2: false,
+      3: false
+    }
+
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
-    const checkboxes = e.target
-    .parentElement
-    .parentElement
-    .parentElement
-    .parentElement
-    .querySelectorAll(".checkbox__element");
+    const target = e.target;
+    const name = target.name;
+    const value = target.checked;
 
-    const checkedCheckboxes = [...checkboxes].filter((checkbox)=> checkbox.checked);
-    const checkedNames = checkedCheckboxes.map((checkbox)=>checkbox.name);
+    const checkedNames = [];
 
-    this.props.onFilterChange(checkedNames);
+    this.setState({
+      [name]: value
+    }, _ => {
+      const state = this.state;
+
+      for (let key in state) {
+        if (state[key]) {
+          checkedNames.push(key);
+        }
+      }
+
+      this.props.onFilterChange(checkedNames);
+    });
   }
 
   handleClick(e) {
-    const targetCheckbox = e.target
-    .parentElement
-    .querySelector(".checkbox__element");
+    const target = e.target;
+    const name = target.dataset.name;
+    const state = this.state;
 
-    const targetName = targetCheckbox.name;
+    for (let key in state) {
+      if (key === name) {
+        state[key] = true;
+      } else {
+        state[key] = false;
+      }
+    }
 
-    const allCheckboxes = e.target
-    .parentElement
-    .parentElement
-    .parentElement
-    .querySelectorAll(".checkbox__element");
-
-    [...allCheckboxes].forEach((checkbox) => {
-      checkbox.checked = false;
+    this.setState({ state }, _ => {
+      this.props.onFilterChange([name]);
     });
-    targetCheckbox.checked = true;
-
-    this.props.onFilterChange([targetName]);
   }
   
   render() {
@@ -57,12 +70,13 @@ class Filter extends Component {
                     className="checkbox__element"
                     name="all"
                     type="checkbox"
+                    checked={this.state.all}
                     onChange={this.handleChange}
                   />
                   <div className="checkbox__check"></div>
                 </label>
               </div>
-              <button className="filter__button" onClick={this.handleClick}>Только</button>
+              <button className="filter__button" onClick={this.handleClick} data-name="all">Только</button>
             </div>
             <div className="filter__item">
               <div className="checkbox">
@@ -71,12 +85,13 @@ class Filter extends Component {
                     className="checkbox__element"
                     name="0"
                     type="checkbox"
+                    checked={this.state[0]}
                     onChange={this.handleChange}
                   />
                   <div className="checkbox__check"></div>
                 </label>
               </div>
-              <button className="filter__button" onClick={this.handleClick}>Только</button>
+              <button className="filter__button" onClick={this.handleClick} data-name="0">Только</button>
             </div>
             <div className="filter__item">
               <div className="checkbox">
@@ -85,12 +100,13 @@ class Filter extends Component {
                     className="checkbox__element"
                     name="1"
                     type="checkbox"
+                    checked={this.state[1]}
                     onChange={this.handleChange}
                   />
                   <div className="checkbox__check"></div>
                 </label>
               </div>
-              <button className="filter__button" onClick={this.handleClick}>Только</button>
+              <button className="filter__button" onClick={this.handleClick} data-name="1">Только</button>
             </div>
             <div className="filter__item">
               <div className="checkbox">
@@ -99,12 +115,13 @@ class Filter extends Component {
                     className="checkbox__element"
                     name="2"
                     type="checkbox"
+                    checked={this.state[2]}
                     onChange={this.handleChange}
                   />
                   <div className="checkbox__check"></div>
                 </label>
               </div>
-              <button className="filter__button" onClick={this.handleClick}>Только</button>
+              <button className="filter__button" onClick={this.handleClick} data-name="2">Только</button>
             </div>
             <div className="filter__item">
               <div className="checkbox">
@@ -113,12 +130,13 @@ class Filter extends Component {
                     className="checkbox__element"
                     name="3"
                     type="checkbox"
+                    checked={this.state[3]}
                     onChange={this.handleChange}
                   />
                   <div className="checkbox__check"></div>
                 </label>
               </div>
-              <button className="filter__button" onClick={this.handleClick}>Только</button>
+              <button className="filter__button" onClick={this.handleClick} data-name="3">Только</button>
             </div>
           </div>
         </div>
